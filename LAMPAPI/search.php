cc765de3b431322848f5c11baa02ea7499bdc60b
Ricardo Ruiz <kxngvenom@gmail.com>
2021-05-25 21:05:59 -0400

    [ADD] Search endpoint (Not tested yet)

--- LAMPAPI/search.php
+++ LAMPAPI/search.php
@@ -4,24 +4,61 @@
   //Get Request Data
   $reqData = getRequestInfo();
   //User will search by either first and last name/phone/address
-  $firstName = $reqData['fname'];
-  $lastName = $reqData['lname'];
-  $phone = $reqData["phone"];
-  $address = $reqData["address"];
+  $search = $reqData["search"];
+  $u_id = $reqData["u_id"];
 
   $conn = new mysqli($server, $dbUsername, $dbPassword, $dbname);
   if($conn->connect_error){
     returnError($conn->connect_error);
   }else{
-    $stmt = $conn->prepare("SELECT * FROM Contacts WHERE Contacts.u_id = $u_id
-      AND ((Contacts.fname LIKE %$firstName% AND Contacts.lname LIKE %lastName%)
-      OR Contacts.phone LIKE %$phone% OR Contacts.address LIKE %$address%)");
+    $stmt = $conn->prepare("SELECT * FROM Contacts WHERE Contacts.u_id=?
+      AND ((Contacts.fname LIKE %?% OR Contacts.lname LIKE %?%)
+      OR Contacts.phone LIKE %?% OR Contacts.address LIKE %?%)");
+    $stmt->bind_param("issss", $u_id, $search, $search, $search, $search);
     $execResult = $stmt->execute();
-
 
     if( false===$execResult ){
       returnError( $stmt->error );
     }
 
+    $result = $stmt->get_result();
+    $searchResult = "";
+    $searchCount = 0;
+    
+    while($row = $result->fetch_assoc()){
+      if($searchCount > 0){
+        $searchResult .= ",";
+      }
+      $searchResult .= json_encode($row);
+    }
+
+    if($searchCount == 0){
+      returnError("No Results Found");
+    }else{
+      returnInfo($searchResult);
+    }
+    
     $stmt->close();
     $conn->close();
+  }
+
+    function getRequestInfo (){
+      return json_decode(file_get_contents('php://input'), true);
+    }
+  
+    function sendResponse ( $response ){
+      header('Content-type: application/json');
+      echo $response;
+    }
+
+    function returnInfo( $searchResult ){
+      $finalRes = '{"results":[' . $searchResult . ']}';
+      printf("%s", $finalRes)
+      sendResponse($finalRes);
+    }
+  
+    function returnError ( $err ){
+      $returnValue = '{"error":"' . $err . '"}';
+      sendResponse($returnValue);
+    }
+?>
