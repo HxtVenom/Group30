fbd1035f5f0bb4eb9159fdde24f6bccbeb84b58e
Ricardo Ruiz <kxngvenom@gmail.com>
2021-06-03 15:55:37 -0400

    [ADD] getContact which gets a contact information by ID

---
+++ LAMPAPI/getContact.php
@@ -0,0 +1,44 @@
+<?php
+  require ('./lib/db.php');
+  require ('./lib/helper.php');
+
+  //Get Request Data
+  $reqData = getRequestInfo();
+  //Get Contact by userID and contactID
+  $c_id = $reqData["c_id"];
+  $u_id = $reqData["u_id"];
+
+  $conn = new mysqli($server, $dbUsername, $dbPassword, $dbname);
+  if($conn->connect_error){
+    returnError($conn->connect_error);
+  }else{
+    $stmt = $conn->prepare("SELECT * FROM Contacts WHERE u_id=? AND c_id=?");
+    $stmt->bind_param('ii', $u_id, $c_id);
+    $execResult = $stmt->execute();
+
+    if( false===$execResult ){
+      returnError( $stmt->error );
+    }
+
+    $result = $stmt->get_result();
+    $searchResult = ""
+
+    if($row = $result->fetch_assoc()){
+      $searchResult .= json_encode($row);
+    }
+
+    if($searchResult == ""){
+      http_response_code(404);
+      returnError("No Contact Found");
+    }else{
+      returnInfo($searchResult);
+    }
+
+    $stmt->close();
+    $conn->close();
+  }
+
+    function returnInfo( $searchResult ){
+      sendResponse($searchResult);
+    }
+?>
