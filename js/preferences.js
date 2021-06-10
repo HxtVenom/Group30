270b396156104410774c0a06ed2c77ed0f6218e2
Michael Sedlack <Michael.sedlack@knights.ucf.edu>
2021-06-09 23:18:24 -0400

    Update preferences.js

--- js/preferences.js
+++ js/preferences.js
@@ -151,7 +151,7 @@ function changePassword() {
 	}
 	
 	var jsonPayload = JSON.stringify({u_id, oldPassword, newPassword1});
-	var url = urlBase + '/changeEmail.' + extension;
+	var url = urlBase + '/changePassword.' + extension;
 	var xhr = new XMLHttpRequest
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
