a34a3384abcd2d9bf42880f0a33a94c3cf2fa609
Ricardo Ruiz <kxngvenom@gmail.com>
2021-06-09 20:39:45 -0400

    [ADD] changeEmail and changePassword

--- LAMPAPI/changeEmail.php
+++ LAMPAPI/changeEmail.php
@@ -1,3 +1,34 @@
 <?php 
+  require ("./lib/db.php");
+  require ("./lib/helper.php");
 
+  $reqData = getRequestInfo();
+
+  $email = $reqData["email"];
+  $u_id = $reqData["u_id"];
+
+  $conn = new mysqli($server, $dbUsername, $dbPassword, $dbname);
+  if($conn->connect_error)
+  {
+    returnError($conn->connect_error);
+  }
+  else
+  {
+    //  sql update logic
+    $stmt = $conn->prepare("UPDATE Users SET email=? WHERE u_id=?");
+    $stmt->bind_param("si", $email, $u_id);
+    $execResult = $stmt->execute();
+
+
+    if( false===$execResult ){
+      http_response_code(400);
+      returnError( $stmt->error );
+    }else{
+      http_response_code(200);
+      returnSuccess("Email successfully updated!");
+    }
+
+    $stmt->close();
+    $conn->close();
+  }
 />
--- LAMPAPI/changePassword.php
+++ LAMPAPI/changePassword.php
@@ -1,3 +1,33 @@
 <?php 
+  require ("./lib/db.php");
+  require ("./lib/helper.php");
 
+  $u_id = $reqData["u_id"];
+  $oldPassword = $reqData["oldPassword"];
+  $newPassword = $reqData["newPassword"];
+
+  $conn = new mysqli($server, $dbUsername, $dbPassword, $dbname);
+  if($conn->connect_error)
+  {
+    returnError($conn->connect_error);
+  }
+  else
+  {
+    //  sql update logic
+    $stmt = $conn->prepare("UPDATE Users SET password=? WHERE u_id=? AND password=?");
+    $stmt->bind_param("sis", $newPassword, $u_id, $oldPassword);
+    $execResult = $stmt->execute();
+
+
+    if( false===$execResult ){
+      http_response_code(400);
+      returnError( $stmt->error );
+    }else{
+      http_response_code(200);
+      returnSuccess("Password successfully updated!");
+    }
+
+    $stmt->close();
+    $conn->close();
+  }
 />
