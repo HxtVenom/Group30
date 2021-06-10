ecc1324d1e47ceb53075ea1ba840cde7dcb39ddb
Michael Sedlack <Michael.sedlack@knights.ucf.edu>
2021-06-10 02:06:41 -0400

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -269,7 +269,7 @@ function openHamburger(x) {
   var links = document.getElementById("myLinks");
   if (links.style.opacity === "1") {
     links.style.opacity = "0";
-	setTimeout(opacityToZeroWorkabout(), 1000);
+	setTimeout(function() {opacityToZeroWorkabout();}, 1000);
   } else {
     links.style.opacity = "1";
 	links.style.display = "block";
